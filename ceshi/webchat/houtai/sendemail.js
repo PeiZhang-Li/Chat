57cfc348d8e99750f918b2b07ac7d5a6ff2a937b
e <593872818@qq.com>
2020-01-02 18:16:37 +0800

    第四次提交

--- ceshi/webchat/houtai/sendemail.js
+++ ceshi/webchat/houtai/sendemail.js
@@ -20,7 +20,7 @@ let email = {
         let nows = nowstr.substring(nowstr.length - 6, nowstr.length);
 
         let mailOpt = {
-            from: `"test" <${emailUrl}>`, // 你到qq邮箱地址
+            from: `"WebChat" <${emailUrl}>`, // 你到qq邮箱地址
             to: '593872818@qq.com', // 接受人,可以群发填写多个逗号分隔
             subject: '重置您的账户密码', // 主题名(邮件名)
             // 可以发送text或者html格式,2选1
